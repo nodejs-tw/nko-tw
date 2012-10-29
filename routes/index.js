a3c3c2e294a77ea3a270e085990973d3e9d98769
Caesar Chi <caesar@mitac.com.tw>
2012-10-29 08:43:05 +0800

    update js, html, css on new nodeko version.

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,6 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Node.js Knockout 台灣黑客頌 - 48 小時不間斷挑戰' });
 };
+exports.new = function(req, res){
+  res.render('new', { title: 'Node.js Knockout 台灣黑客頌 - 48 小時不間斷挑戰' });
+};
