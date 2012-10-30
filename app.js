da73a36ac633bdce6d3d21e2ee9723b836c0c494
Caesar Chi <caesar@mitac.com.tw>
2012-10-30 19:37:40 +0800

    update server

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/new', routes.new);
+/*
+app.get('/agenda', routes.agenda);
+*/
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
