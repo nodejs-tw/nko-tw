ee873604fffe47b97abd79f8a75f023db2c4e1e6
Caesar Chi <caesar@mitac.com.tw>
2012-10-31 13:23:08 +0800

    update agenda

--- app.js
+++ app.js
@@ -32,11 +32,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 app.get('/new', routes.new);
-/*
 app.get('/agenda', routes.agenda);
-*/
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,6 @@ exports.index = function(req, res){
 exports.new = function(req, res){
   res.render('new', { title: 'Node.js Knockout 台灣黑客頌 - 48 小時不間斷挑戰' });
 };
-/*
 exports.agenda = function(req, res){
   res.render('agenda', { title: 'Node.js Knockout 台灣黑客頌 - 48 小時不間斷挑戰' });
 };
-*/
