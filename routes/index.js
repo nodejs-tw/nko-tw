03b4be4e4d0efdd69f88b4f074feff16ea2cf01f
Caesar Chi <caesar@mitac.com.tw>
2012-11-11 13:03:00 +0800

    update judge view and link

--- routes/index.js
+++ routes/index.js
@@ -12,3 +12,6 @@ exports.new = function(req, res){
 exports.agenda = function(req, res){
   res.render('agenda', { title: 'Node.js Knockout 台灣黑客頌 - 48 小時不間斷挑戰' });
 };
+exports.judge = function(req, res){
+  res.render('judge', { title: 'Node.js Knockout 台灣黑客頌 - 48 小時不間斷挑戰' });
+};
